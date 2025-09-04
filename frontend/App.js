48ae2f89a50528cb2b2106050d25ab1c2f47cd68
TobiasDaniel <jtdacosta123@gmail.com>
2025-09-03 23:14:42 -0300

    cambios

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import logo from "./assets/favicon.png";//importar img
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HOLAA TOBIAS!</Text>
-      <Text>HOLAA TOBIAS!</Text>
+      {/**<Image source={logo} style={{width: 70, height: 70}}></Image>/**en la direccion pongo lo importado*/}
+      <Image source={{ uri:"https://img.freepik.com/vector-gratis/cartel-reciclaje-tiene-hojas-planas_78370-5578.jpg"}} style={{width: 70, height: 70}}/>
+      <Text style={{ color: "white" }}><h1>Bienvenidos a EcoRed</h1></Text>
+      <Button color="blue" title='PULSE AQUI' onPress={()=> alert("Hola!")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#33e242ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
