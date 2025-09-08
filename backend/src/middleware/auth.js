717c1f94226f657b220d2782afb08e3042dd1a4f
kelm-ismael <kelmismael@gmail.com>
2025-09-07 22:23:03 -0300

    bbbb

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,21 +1,4 @@
-// import jwt from 'jsonwebtoken';
-// import dotenv from 'dotenv';
 
-// dotenv.config();
-
-// export function verificarToken(req, res, next) {
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader && authHeader.split(' ')[1];
-
-//   if (!token) return res.status(401).json({ error: 'Token requerido' });
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) return res.status(403).json({ error: 'Token inválido' });
-
-//     req.usuario = decoded;
-//     next();
-//   });
-// }
 
 import jwt from 'jsonwebtoken';
 
--- backend/src/models/usuario.model.js
+++ backend/src/models/usuario.model.js
@@ -1,31 +1,3 @@
-// import db from '../config/db.js';
-
-// export async function obtenerUsuarios() {
-//   const [rows] = await db.query(`SELECT * FROM usuario`); //consulta sql va entre las comillas
-//   return rows;
-// }
-
-// export async function insertarUsuario(usuario) {
-//   const { email, contrasenia_hash, id_tipo_usuario, id_referencia } = usuario;
-//   const [result] = await db.query(
-//     `INSERT INTO usuario (email, contrasenia_hash, id_tipo_usuario, id_referencia) VALUES (?, ?, ?, ?)`,
-//     [email, contrasenia_hash, id_tipo_usuario, id_referencia]
-//   );
-//   return result.insertId;
-// }
-
-// export async function editarUsuario(id, datos) {
-//   const { email, estado } = datos;
-//   await db.query(
-//     `UPDATE usuario SET email = ?, estado = ? WHERE id = ?`,
-//     [email, estado, id]
-//   );
-// }
-
-// export async function borrarUsuario(id) {
-//   await db.query(`DELETE FROM usuario WHERE id = ?`, [id]);
-// }
-
 
 // backend/src/models/usuario.model.js
 import db from '../config/db.js';
