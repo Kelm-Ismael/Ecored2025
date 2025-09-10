38f7d5110b54658dfb4d3b01f59619d9087d4282
TobiasDaniel <jtdacosta123@gmail.com>
2025-09-09 22:01:50 -0300

    nada

--- frontend/screens/informacion.js
+++ frontend/screens/informacion.js
@@ -1,17 +1,52 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Button , StyleSheet} from 'react-native-web';
 
-import { commonStyles } from '../styles/styles';
+
 
 export default function ScreenInformacion() {
     return (
-        <SafeAreaView style={commonStyles.safeArea}>
-            <View style={commonStyles.container}>
-                <Text style={commonStyles.title}>
+        <SafeAreaView >
+            <View style={styles.container}>
+                <Text>
                     Informacion
                 </Text>
+                <TouchableOpacity style={{gap: 15}}>
+                    <TouchableOpacity style={styles.btn} onPress={() => alert("hola")}>
+                        <Text style={styles.textoBoton}>Link Info Reciclaje</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => alert("hola")}>
+                        <Text style={styles.textoBoton}>Link Info Reciclaje</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => alert("hola")}>
+                        <Text style={styles.textoBoton}>Link Info Reciclaje</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => alert("hola")}>
+                        <Text style={styles.textoBoton}>Link Info Reciclaje</Text>
+                    </TouchableOpacity>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: "#f7fff6ff",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20, // separa los botones
+    },
+    btn: {
+        borderRadius: 20,
+        backgroundColor: "#51cf45ff",
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+    },
+    TextoBoton: {
+        color: "#117911ff",
+        fontSize: 16,
+        fontWeight: "bold",
+    }
+});
