8f7453d1a866f975a55a2dea6a783b3a089d5649
Caro Okada Echevarría (Charlie) <86701447+carokada@users.noreply.github.com>
2025-09-13 19:46:35 -0300

    Delete frontend/config/api.js
    delete file

--- frontend/config/api.js
+++
@@ -1,6 +0,0 @@
-const LOCAL = 'http://localhost:3000'; // cuando estás en desarrollo
-const LAN = 'http://192.168.100.7:3000'; // IP de tu PC en la red WiFi
-const REMOTO = 'https://midominio.com'; // si tenés un deploy
-
-export const BASE_URL = LAN;
-console.log(BASE_URL)
